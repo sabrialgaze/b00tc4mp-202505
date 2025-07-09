0bd5dab72074c7b59da3a08625fc8cc10b1b967f
Sabrina Algaze <sabrina.algaze@gmail.com>
2025-07-09 10:33:04 +0200

    add search route to filter cameras by brand based on query parameter q; add a json file with camera models; #2

--- staff/sabrina-algaze/playground/web-server/index.js
+++ staff/sabrina-algaze/playground/web-server/index.js
@@ -10,11 +10,20 @@ server.get('/', (request, response) => {
 server.get('/search', (request, response) => {
     const { q } = request.query
 
-    const cameras = data.loadCameras()
+    let cameras = data.loadCameras()
 
     //TODO filter cameras according to query (q)
+    cameras = cameras.filter(camera => camera.brand.toLowerCase() === q.toLowerCase())
 
-    response.send(cameras)
+    response.send(cameras.map(camera => {
+        return `<ul>
+        <li>Brand: ${camera.brand}</li>
+        <li>Model: ${camera.model}</li>
+        <li>Type: ${camera.type}</li>
+        <li>Film format: ${camera.film_format}</li>
+        <li>Year: ${camera.year}</li>
+        </ul>`
+    }).join(''))
 })
 
 server.listen(8080, () => console.log('Server is up on port 8080'))
